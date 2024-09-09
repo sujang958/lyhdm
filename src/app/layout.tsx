5f23356f2b33940e87ff19600cdab85f0c5fc157
sujang958 <sujangdev@gmail.com>
2024-09-09 23:13:06 +0900

    feat: add a button

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next"
+import "./globals.css"
+// export const metadata: Metadata = {
+//   title: "",
+//   description: "",
+// }
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className="font-pretendard">{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/Button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button>HEllo</Button>
+    </div>
+  )
+}
